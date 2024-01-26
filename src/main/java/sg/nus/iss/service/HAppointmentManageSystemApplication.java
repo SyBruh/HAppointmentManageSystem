b1fb5873b3ce1321ec5d4a0ef5eea8f687b1107a
Swam Yay Pyae Hmuu <swamyaypyaehmuu@gmail.com>
2024-01-26 22:49:57 +0800

    first

---
+++ src/main/java/sg/nus/iss/service/HAppointmentManageSystemApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HAppointmentManageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HAppointmentManageSystemApplication.class, args);
+	}
+
+}
